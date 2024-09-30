a5c5136cdc7c3bb32639b84c011fb2b28a38f90a
Ashish Das <ashish09@cisco.com>
2024-09-30 06:06:27 +0000

    3.0.0 release

--- Package.swift
+++ Package.swift
@@ -23,23 +23,23 @@ let package = Package(
   targets: [
       .binaryTarget(
           name: "WebexConnectCore",
-          url: "https://github.com/webex/wxconnect-ios-sdk/releases/download/3.0.0/WebexConnectCore.zip",
-          checksum: "1ba16d2b4e70231f7fcbe6d2671325eeb1f0bdcd5273dc248daf7f5ae87b39a3"
+          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.0/WebexConnectCore.zip",
+          checksum: "209496c7e41b0fa2fed1fac1f755c947ad8d18461e855c95cc5f3dee7a306e92"
       ),
       .binaryTarget(
           name: "WebexConnectPush",
-          url: "https://github.com/webex/wxconnect-ios-sdk/releases/download/3.0.0/WebexConnectPush.zip",
-          checksum: "b6eb07ee4a900584d3cc2d2b8d26e3899d9e7e664c1c5cdbc720009107918f51"
+          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.0/WebexConnectPush.zip",
+          checksum: "e29bef2afa30fd09225d25f0b57dcf978af2b3afc9125e99c877283d0764dc07"
       ),
     .binaryTarget(
           name: "WebexConnectInAppMessaging",
-          url: "https://github.com/webex/wxconnect-ios-sdk/releases/download/3.0.0/WebexConnectInAppMessaging.zip",
-          checksum: "944bf942cc569e6fa32bb8ef53574ac9b7af5092d01834981aabb7dfc0075518"
+          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.0/WebexConnectInAppMessaging.zip",
+          checksum: "970d40977d60a2459908da497b2ec6dff8f3ab5d08b60efadf5b9bbcdbc5d482"
       ),
       .binaryTarget(
           name: "WebexConnectNotificationServiceExtension",
-          url: "https://github.com/webex/wxconnect-ios-sdk/releases/download/3.0.0/WebexConnectNotificationServiceExtension.zip",
-          checksum: "5f9c4b9f84f0d14b73d468bd3f0e4a3605ba6c6cf4eaf8f8c66ccd33e704d5c3"
+          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.0/WebexConnectNotificationServiceExtension.zip",
+          checksum: "091c06f41cd390974232074c87c7585326d3068f8373f65bda49770282527473"
       )
   ]
 )
