88ed1513f81e18125124a024b8143def8634a188
Ashish Das <ashish09@cisco.com>
2025-12-04 03:36:18 +0000

    3.0.6 release

--- Package.swift
+++ Package.swift
@@ -26,23 +26,23 @@ let package = Package(
   targets: [
       .binaryTarget(
           name: "WebexConnectCore",
-          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.5/WebexConnectCore.zip",
-          checksum: "d90affaef0320ae9ba417d35b89d43f4e7b880de5a3fe32eb041731b56e478a9"
+          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.6/WebexConnectCore.zip",
+          checksum: "09b0ce8b066b7da5619fe554b757d32ec78f6f8dc825e02d94d5ddd65fe9df12"
       ),
       .binaryTarget(
           name: "WebexConnectPush",
-          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.5/WebexConnectPush.zip",
-          checksum: "c975dc092ab1475c24a21c6b8175863371a7ecefa1a7d5e0eed76382f7a1fb65"
+          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.6/WebexConnectPush.zip",
+          checksum: "d91c530e3657366c8a410ea3a6047902a21d25a924d77006b584f58c50843be6"
       ),
     .binaryTarget(
           name: "WebexConnectInAppMessaging",
-          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.5/WebexConnectInAppMessaging.zip",
-          checksum: "cea1d3d812f5503a860535e1364efc676ea1c00901920f49a031b53d494357a7"
+          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.6/WebexConnectInAppMessaging.zip",
+          checksum: "30d99696b2a59d6f59b3b0b8c76bc84b6d8d91f1ed6d5cf13d6f1213895ac8be"
       ),
       .binaryTarget(
           name: "WebexConnectNotificationServiceExtension",
-          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.5/WebexConnectNotificationServiceExtension.zip",
-          checksum: "38d95621aa10bd1562ec328c133815140ab87c666c980f8a3b819d845d3c8cf9"
+          url: "https://github.com/webex/webexconnect-ios-sdk/releases/download/3.0.6/WebexConnectNotificationServiceExtension.zip",
+          checksum: "9632becb3452864b94e5704f77d5edeea06920c413931b2010d7f50b4230b4f3"
       ),
       .target(
             name: "WebexConnectCoreWrapper",
